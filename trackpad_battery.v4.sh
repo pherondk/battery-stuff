967da385719d4172ad6f7bd94a216b9b846e0851
heretic <heretic@celticlich.com>
2020-06-02 18:42:36 -0500

    cleanup

--- trackpad_battery.v4.sh
+++ trackpad_battery.v4.sh
@@ -1,13 +1,17 @@
 
 #!/bin/bash
 
-BATTLVL=$(ioreg -r -l -n AppleHSBluetoothDevice | egrep '"BatteryPercent" = |^  \|   "Bluetooth Product Name" = '| sed 's/  |   "Bluetooth Product Name" = "Magic Keyboard with Numeric Keypad"/Keyboard:/'| sed 's/  |   "Bluetooth Product Name" = "Magic Trackpad 2"/Trackpad:/'| sed 's/  |   |       "BatteryPercent" = / /'); #echo $BATTLVL
+BATTLVL=$(ioreg -r -l -n AppleHSBluetoothDevice \
+		| egrep '"BatteryPercent" = |^  \|   "Bluetooth Product Name" = '\
+		| sed 's/  |   "Bluetooth Product Name" = "Magic Keyboard with Numeric Keypad"/Keyboard:/'\
+		| sed 's/  |   "Bluetooth Product Name" = "Magic Trackpad 2"/Trackpad:/'\
+		| sed 's/  |   |       "BatteryPercent" = / /'); #echo $BATTLVL
 
 { read a; read b; read c; read d; } <<< "${BATTLVL}"
 
 if [[ $a == *"Trackpad"*  ]] && [[ $c == *"Keyboard"* ]]; then
 		BATTPERC=$(echo $a $b); echo $BATTPERC;
-elif [[ $a == *"Trackpad"* ]] && [[ $c == "" ]]; then
+elif [[ $a == *"Trackpad"* ]] && [[ -z $c ]]; then
 		BATTPERC=$(echo $a $b); echo $BATTPERC;
 elif [[ $a == *"Keyboard"* ]] && [[ $c == *"Trackpad"* ]]; then
 		BATTPERC=$(echo $c $d); echo $BATTPERC;
