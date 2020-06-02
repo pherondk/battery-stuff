4aa18011da4f29f7d1b656798cac1d1a142cb6f1
heretic <heretic@celticlich.com>
2020-06-02 18:13:07 -0500

    clean up

--- keyboard.v4.sh
+++ keyboard.v4.sh
@@ -1,7 +1,11 @@
 
 #!/bin/bash
 
-BATTLVL=$(ioreg -r -l -n AppleHSBluetoothDevice | egrep '"BatteryPercent" = |^  \|   "Bluetooth Product Name" = '| sed 's/  |   "Bluetooth Product Name" = "Magic Keyboard with Numeric Keypad"/Keyboard:/'| sed 's/  |   "Bluetooth Product Name" = "Magic Trackpad 2"/Trackpad:/'| sed 's/  |   |       "BatteryPercent" = / /'); #echo $BATTLVL
+BATTLVL=$(ioreg -r -l -n AppleHSBluetoothDevice \
+		| egrep '"BatteryPercent" = |^  \|   "Bluetooth Product Name" = '\
+		| sed 's/  |   "Bluetooth Product Name" = "Magic Keyboard with Numeric Keypad"/Keyboard:/'\
+		| sed 's/  |   "Bluetooth Product Name" = "Magic Trackpad 2"/Trackpad:/'\
+		| sed 's/  |   |       "BatteryPercent" = / /'); #echo $BATTLVL
 
 { read a; read b; read c; read d; } <<< "${BATTLVL}"
 
